6e8b827b8256b4c0f99893811eb91eb99d9dad4b
Nemixs <nemixs@hotmail.cl>
2017-08-30 13:43:07 -0300

    Botones

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Bootstrap 101 Template</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -20,9 +20,27 @@
   <body>
     <h1>Hello, world!</h1>
 
+    <div class="row">
+      <div class="col-md-12">
+        <a class="btn btn-default" href="#" role="button">7</a>
+        <a class="btn btn-default" href="#" role="button">8</a>
+        <a class="btn btn-default" href="#" role="button">9</a>
+      </div>
+      <div class="col-md-12">
+        <a class="btn btn-default" href="#" role="button">4</a>
+        <a class="btn btn-default" href="#" role="button">5</a>
+        <a class="btn btn-default" href="#" role="button">6</a>
+      </div>
+      <div class="col-md-12">
+        <a class="btn btn-default" href="#" role="button">1</a>
+        <a class="btn btn-default" href="#" role="button">2</a>
+        <a class="btn btn-default" href="#" role="button">3</a>
+      </div>
+    </div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
   </body>
 </html>
