de157d30addd18268e5ed2dbcbec6a8fa7ee08ed
Nemixs <nemixs@hotmail.cl>
2017-08-30 20:28:34 -0300

    Triángulo
    Se programa funcionalidad base del Sistema
    Se deasrrolla el funcionamiento lógico del cálculo de área del Triángulo
    
    Trabajo con JavaScript -> funciones en tiempo real
    
    Se quitan temporalmente las botoneras de números

---
+++ assets/js/funciones.js
@@ -0,0 +1,64 @@
+function ingresarNumero(numero){
+	document.getElementById("input").value = document.getElementById("input").value + (numero);
+}
+
+function activarBoton(option){
+	if (option == 1){
+		/* Activación y desactivación de buttons */
+		$(document.getElementById("triangulo")).addClass('active');
+		$(document.getElementById("cuadrado")).removeClass('active');
+		$(document.getElementById("circulo")).removeClass('active');
+
+		/* Edición de textos en input principal */
+		$(document.getElementById("label")).html("Ingrese Altura Triángulo");
+		document.getElementById("input").placeholder = "Ingrese Altura Triángulo";
+		document.getElementById("forma").value = "1";
+	}
+	else if (option == 2){
+		/* Activación y desactivación de buttons */
+		$(document.getElementById("triangulo")).removeClass('active');
+		$(document.getElementById("cuadrado")).addClass('active');
+		$(document.getElementById("circulo")).removeClass('active');
+	}
+	else if (option == 3){
+		/* Activación y desactivación de buttons */
+		$(document.getElementById("triangulo")).removeClass('active');
+		$(document.getElementById("cuadrado")).removeClass('active');
+		$(document.getElementById("circulo")).addClass('active');
+	}
+}
+
+function ingresar(){
+	event.preventDefault();
+	if (document.getElementById("input").value){
+		var action = 0;
+		if (document.getElementById("forma").value == 1 && document.getElementById("hide").value == 1){
+			$(document.getElementById("A")).html(document.getElementById("input").value);
+
+			/* Limpieza de atributos en caso de segundo uso */
+			$(document.getElementById("B")).html(0);
+			document.getElementById("hide").value = 2;
+			document.getElementById("input").value = "";
+
+			$("#resultado_div").hide();
+			action = 1;
+		} else if (document.getElementById("forma").value == 1 && document.getElementById("hide").value == 2 && action == 0){
+			$(document.getElementById("B")).html(document.getElementById("input").value);
+			document.getElementById("hide").value = 3;
+			document.getElementById("input").value = "";
+
+			action = 1;
+		}
+	}
+	if (document.getElementById("forma").value == 1 && document.getElementById("hide").value == 3){
+		$("#resultado_div").show();
+		$(document.getElementById("ResultadoArea")).html((document.getElementById("A").innerHTML * document.getElementById("B").innerHTML) / 2);
+		$(document.getElementById("ResultadoPerimetro")).html("Falta Sacar Perímetro");
+		document.getElementById("hide").value = 1;
+	}
+}
+
+
+function cargar(){
+    $("#resultado_div").hide();
+}
--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Calculadora - Testing</title>
 
     <!-- Bootstrap -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/js/funciones.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,42 +18,124 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body onload="cargar()"> <!-- onload="cargar()" -->
+    <div class="text-center"> <!-- <div align="center"> -->
+      <h1>Calculadora de Área y Perímetro - Triángulos</h1>
+      <br>
+          </div>
 
-    <div >
+    <br>
+
+    <div class="text-center">
       <div class="row">
-        <div class="col-md-12">
-          <div class="col-md-6">
-            <a class="btn btn-default btn-lg" href="#" role="button">7</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">8</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">9</a>
-          </div>
+        <input type="hidden" id="forma" value="1">
+        <div class="col-md-2 col-md-offset-1">
+          <button type="button" class="btn btn-success btn-lg btn-block active" id="triangulo" onclick="activarBoton(1)">Triángulo</button>
         </div>
-        <div class="col-md-12">
-          <div class="col-md-6">
-            <a class="btn btn-default btn-lg" href="#" role="button">4</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">5</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">6</a>
-          </div>
+        <div class="col-md-2 col-md-offset-2">
+          <button type="button" class="btn btn-primary btn-lg btn-block" id="cuadrado" onclick="activarBoton(2)">Cuadrado</button>
         </div>
-        <div class="col-md-12">
-          <div class="col-md-6">
-            <a class="btn btn-default btn-lg" href="#" role="button">1</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">2</a>
-            <a class="btn btn-default btn-lg" href="#" role="button">3</a>
-          </div>
-        </div>
-        <div class="col-md-12">
-          <div class="col-md-6">
-            <a class="btn btn-default btn-lg" href="#" role="button">0</a>
-          </div>
+        <div class="col-md-2 col-md-offset-2">
+          <button type="button" class="btn btn-danger btn-lg btn-block" id="circulo" onclick="activarBoton(3)">Círculo</button>
         </div>
       </div>
+      <br><br>
+      
+      <form>
+        <div class="col-md-12">
+          <div class="input-group col-md-6 col-md-offset-3">
+            <span class="input-group-addon" id="label">Ingrese Altura Triángulo</span>
+            <input type="text" class="form-control input-lg" id="input" placeholder="Ingrese Altura Triángulo">
+          </div>
+    <!--  <div class="input-group col-md-6">
+            <button class="btn btn-default btn-lg">Ingresar</button>
+          </div> -->
+        </div>
+        
+        <br><br><br><br>
+        <!--
+        <div class="row">
+          <div class="col-md-12">
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(7)">7</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(8)">8</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(9)">9</button>
+          </div>
+          <div class="col-md-12">
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(4)">4</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(5)">5</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(6)">6</button>
+          </div>
+          <div class="col-md-12">
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(1)">1</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(2)">2</button>
+            <button class="btn btn-default btn-lg" onclick="ingresarNumero(3)">3</button>
+          </div>
+          <div class="col-md-12">
+              <button class="btn btn-default btn-lg" onclick="ingresarNumero(0)">0</button>
+          </div>
+        </div>
+        -->
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="col-md-2 col-md-offset-4">
+              <h3>Altura Triángulo</h3>
+            </div>
+            <div>
+              <h3 class="col-md-2" id="A">0</h3>
+            </div>
+            <div class="col-md-2 col-md-offset-4">
+              <h3>Base Triángulo</h3>
+            </div>
+            <div>
+              <h3 class="col-md-2" id="B">0</h3>
+            </div>
+            <div id="resultado_div">
+              <div class="col-md-2 col-md-offset-4">
+                <h3 class="col-md-2">Área</h3> <!-- type="hidden" -->
+              </div>
+              <div>
+                <h3 class="col-md-2" id="ResultadoArea">0</h3>
+              </div>
+              <div class="col-md-2 col-md-offset-4">
+                <h3 class="col-md-2">Perímetro</h3> <!-- type="hidden" -->
+              </div>
+              <div>
+                <h3 class="col-md-2" id="ResultadoPerimetro">0</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <br><br>
+        
+        <div class="input-group col-md-6 col-md-offset-3">
+          <input type="hidden" id="hide" value="1">
+          <button class="btn btn-default btn-lg" onclick="ingresar()" accesskey="intro">Ingresar</button>
+        </div>
+
+      </form>
     </div>
+
+
+
+
+
+
+
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
+
+  <footer class="footer">
+    <div class="container">
+      <br><br>
+      <p class="pull-left">Copyright &copy; Nemixs - <?php echo date("Y/m/d h:i:s A") ?></p>
+    </div>
+</footer>
+
 </html>
