e8d7e87311c2b0d03d35fde952a6e769aa3ee876
Nemixs <nemixs@hotmail.cl>
2017-09-05 01:22:48 -0300

    Fixeos Importantes
    Fixeos de sintaxis {}

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,21 +8,22 @@
     <title>Calculadora - Testing</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="assets/js/funciones.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
-  <body>
+
   <body onload="cargar()"> <!-- onload="cargar()" -->
     <div class="text-center"> <!-- <div align="center"> -->
       <h1>Calculadora de Área y Perímetro </h1>
       <br>
-          </div>
+    </div>
 
     <br>
 
@@ -124,18 +125,9 @@
       </form>
     </div>
 
-
-
-
-
-
-
-
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
   </body>
 
@@ -145,8 +137,5 @@
       <p class="pull-left">Copyright &copy; Nemixs - <?php echo date("Y/m/d h:i:s A") ?></p>
     </div>
 </footer>
-
-</html>
-\ 
 
 </html>
